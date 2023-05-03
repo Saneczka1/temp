8b0f0c45e7dc09be5c266e851002e486812da47d
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-03 22:37:43 +0200

    rename kenel to kernel

--- GpioEmu.v
+++ GpioEmu.v
@@ -107,56 +107,57 @@ end
 always @(posedge clk) begin
     case (state)
         IDLE: begin
-            result = 0;
-            ready = 1'b0;
-            valid = 1'b1;
-            B = 2'b01;
-            done = 0;
-            tmp_ones_count = 0;
-            state = MULT;
+            result <= 0;
+            ready <= 1'b0;
+            valid <= 1'b1;
+            B <= 2'b01;
+            done <= 0;
+            tmp_ones_count <= 0;
+            state <= MULT;
         end
         MULT: begin
-            ready = 0;
-            result = 49'b0;
+            ready <= 0;
+            result <= 49'b0;
             for (integer i = 0; i < 24; i = i + 1) begin
                 if (A2[i]) begin
-                    result = result + ({24'h0, A1} << i);
+                    result <= result + ({24'h0, A1} << i);
                 end
             end
-            valid = (result[48:32] == 0);
-            W = result [31:0];
-            B = {ready, valid};
-            state = COUNT_ONES;
+            valid <= (result[48:32] == 0);
+            W <= result [31:0];
+            B <= {ready, valid};
+            state <= COUNT_ONES;
         end
         COUNT_ONES: begin
-            ready = 0;
-            B = {ready, valid};
-            tmp_ones_count = 0;
+            ready <= 0;
+            B <= {ready, valid};
+            tmp_ones_count <= 0;
             for (integer i = 0; i < 32; i = i + 1) begin
                 if (result[i]) begin
-                    tmp_ones_count = tmp_ones_count + 1;
+                    tmp_ones_count <= tmp_ones_count + 1;
                 end
             end
-            L = tmp_ones_count;
-            B = {ready, valid};
-            state = DONE;
+            L <= tmp_ones_count;
+            B <= {ready, valid};
+            state <= DONE;
         end
         DONE: begin
-            done = 1'b1;
+            done <= 1'b1;
             if (swr && saddress == 16'h03A0) begin // write B
-                B = sdata_in[2:1];
+                B <= sdata_in[2:1];
             end else if (swr && saddress == 16'h0398) begin // write L
-                L = sdata_in[23:0];
+                L <= sdata_in[23:0];
             end else if (swr && saddress == 16'h0390) begin // write W
-                W = sdata_in[31:0];
+                W <= sdata_in[31:0];
             end else begin
-                state = 4;
-                ready = 1'b1;
-                operation_count = operation_count + 1;
+                state <= 4;
+                ready <= 1'b1;
+                operation_count <= operation_count + 1;
             end
         end
     endcase
 end
+
 
 
 assign gpio_out = {16'h0, operation_count[15:0]};

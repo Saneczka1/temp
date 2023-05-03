f1bca3d2730fd5a9f59382e61591e2522c394c6b
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-04 00:03:30 +0200

    change gpioemu and now 85% is good, maybe problems with adresses and gpio out bcs now gpioout is only vounter which has 8 bits

--- GpioEmu.v
+++ GpioEmu.v
@@ -46,15 +46,13 @@ module gpioemu(n_reset,
 
     always @(negedge n_reset) begin
         gpio_in_s <= 0;
-		gpio_in <=0;
         gpio_out_s <= 0;
-		gpio_out <= 0;
         sdata_out_s <= 0;
         state <= 4;
         result <=49'b0;
-		W = 32'b0;
-        tmp_ones_count = 0;
-        operation_count = 0;
+		W <= 32'b0;
+        tmp_ones_count <= 0;
+        operation_count <= 0;
         ready <= 1'b1;
         A1 <= 0;
         A2 <= 0;
@@ -87,7 +85,7 @@ end
 always @(posedge srd) begin
     if (saddress == 16'h390) begin
         if (done) begin
-		W = result[31:0];
+		W <= result[31:0];
             sdata_out_s <= W[31:0];
         end
     end 
@@ -107,59 +105,57 @@ end
 always @(posedge clk) begin
     case (state)
         IDLE: begin
-            result <= 0;
-            ready <= 1'b0;
-            valid <= 1'b1;
-            B <= 2'b01;
-            done <= 0;
+            result = 0;
+            ready = 1'b0;
+            B = 2'b01;
+            done = 0;
             tmp_ones_count = 0;
-            state <= MULT;
+            state = MULT;
         end
         MULT: begin
-            ready <= 0;
-            result <= 49'b0;
+            ready = 0;
             for (integer i = 0; i < 24; i = i + 1) begin
                 if (A2[i]) begin
-                    result <= result + ({24'h0, A1} << i);
+                    result = result + ({24'h0, A1} << i);
                 end
             end
-            valid <= (result[48:32] == 0);
+			
+			valid <= (result[48:32] == 0);
             W = result [31:0];
-            B <= {ready, valid};
-            state <= COUNT_ONES;
+            B = {ready, valid};
+            state = COUNT_ONES;
         end
         COUNT_ONES: begin
-            ready <= 0;
-            B <= {ready, valid};
+            ready = 0;
+            B = {ready, valid};
             tmp_ones_count = 0;
             for (integer i = 0; i < 32; i = i + 1) begin
                 if (result[i]) begin
                     tmp_ones_count = tmp_ones_count + 1;
                 end
             end
-            L <= tmp_ones_count;
-            B <= {ready, valid};
-            state <= DONE;
+            L = tmp_ones_count;
+            B = {ready, valid};
+            state = DONE;
         end
         DONE: begin
-            done <= 1'b1;
+            done = 1'b1;
             if (swr && saddress == 16'h03A0) begin // write B
-                B <= sdata_in[2:1];
+                B = sdata_in[2:1];
             end else if (swr && saddress == 16'h0398) begin // write L
-                L <= sdata_in[23:0];
+                L = sdata_in[23:0];
             end else if (swr && saddress == 16'h0390) begin // write W
                 W = sdata_in[31:0];
             end else begin
-                state <= 4;
-                ready <= 1'b1;
+                state = 4;
+                ready = 1'b1;
                 operation_count = operation_count + 1;
             end
         end
     endcase
 end
 
-
-
+      
 assign gpio_out = {16'h0, operation_count[15:0]};
 assign gpio_in_s_insp = gpio_in_s;
 assign sdata_out = sdata_out_s;
